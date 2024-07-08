5f87ea2949acf81406dd1ec8949fd79f340bb6f3
Darshini <darshinitankala@gmail.com>
2024-07-08 17:24:39 +0530

    checking condition

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let captchachecked = false;
 function beforeSubmit(event)
 {
-    if(captchachecked)
+    if(captchasuccess)
     {
     let outputDate = document.querySelector('.outputDate');
     let inputDate = document.querySelector('.inputDate');
